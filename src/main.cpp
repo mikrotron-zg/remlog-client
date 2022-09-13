7afba5a1fb394f2d4e9148218f511e9fa82e8bce
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-09-13 14:16:30 +0200

    Implement http post for log content

--- src/main.cpp
+++ src/main.cpp
@@ -9,9 +9,19 @@
   * see the project root for license info
   *******************************************************************/
 
+// Includes
 #include <Arduino.h>
 #include <WiFi.h>
+#include <HTTPClient.h>
 #include "Credentials.h" // see CredentialsTemplate.h for instructions first!
+
+// Global variables
+WiFiClient client;
+HTTPClient http;
+const String server_path = "http://" + String(REMLOG_IP_ADDRESS) + ":" +
+                            String(REMLOG_PORT) + "/logs/device/" +
+                            String(DEVICE_ID);
+int counter = 1;
 
 bool connectToWiFi() {
   // Connect to WiFi network
@@ -28,15 +38,44 @@ bool connectToWiFi() {
   return false;
 }
 
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-  Serial.println("Starting...");
-  if (connectToWiFi()) {
-    // TODO startup log
+void log(String content) {
+  // Send log to remlog server
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Not connected to WiFi, trying to reconnect");
+    if (!connectToWiFi()) {
+      Serial.println("Reconnect failed, not sending log!");
+      return;
+    }
   }
+  Serial.print("Sending log content: ");
+  Serial.println(content);
+
+  String json = "{\"content\":\"" + content + "\"}";
+  Serial.print("Formatted JSON: ");
+  Serial.println(json);
+
+  http.begin(client, server_path);
+  http.addHeader("Content-Type", "application/json");
+  int httpResponse = http.POST(json);
+  delay (500);
+  Serial.println("Server response:" + String(httpResponse));
+
+  http.end();
+}
+
+void setup() {
+  // Program start
+  Serial.begin(115200);
+  delay(5000);
+  connectToWiFi();
+  Serial.println("Sending start log to:");
+  Serial.println(server_path);
+  log("Device started at: " + String(millis()));
 }
 
 void loop() {
-  // TODO periodic log
+  // Program loop
+  delay(60000); // wait a minute! ;-)
+  log("Periodic log entry #" + String(counter));
+  counter++;
 }
