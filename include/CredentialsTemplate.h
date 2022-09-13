ff2fd99a58f5a80f024946c006c14b4faf6ad83b
Tomislav Preksavec <tomislav.preksavec@gmail.com>
2022-09-13 11:53:03 +0200

    Add remlog data to credentials template

--- include/CredentialsTemplate.h
+++ include/CredentialsTemplate.h
@@ -7,7 +7,11 @@
  * 1. Save this file as Credentials.h in include/ directory and
  *    than close this file
  * 2. Open Credentials.h file and enter your WiFi credentials and
- *    device Id from remlog server (see remlog documentation)
+ *    device Id from remlog server (see remlog documentation). 
+ *    Add server IP address, it should be your local network address,
+ *    e.g. "192.168.0.101", don't add anything else to the string. 
+ *    By default, remlog listenes on port 8888, if you've 
+ *    configured it differently, change this value too
  * 3. Save this file and compile and upload the project to ESP32
  *    board
  * 
@@ -16,12 +20,18 @@
  * your local version of the project. This way you can change the 
  * project and publish your changes on Github, Gitlab, Bitbucket or
  * your own Git server without leaking sensitive information.
+ * Once you're done, you can delete this comment block from
+ * Credentials.h file.
  *******************************************************************/
 
 #ifndef CREDENTIALS_H
     #define CREDENTIALS_H
-    // Add your AP credentials
-    #define AP_SSID "Your SSID"
-    #define AP_PASSWORD "Your password"
+    // Add your WiFI credentials
+    #define WIFI_SSID "Your SSID"
+    #define WIFI_PASSWORD "Your password"
+    // Add device Id
     #define DEVICE_ID "Remlog device id"
+    // Add remlog server data
+    #define REMLOG_IP_ADDRESS "192.168.0.0"
+    #define REMLOG_PORT "8888" // this is default value, change if needed
 #endif
